55388ed9ec83541f7df72f7870d875fdc7d8f8ec
Amanda Moyo <havocosplay@gmail.com>
2022-09-13 20:15:11 -0400

    doing more in loop

--- chapter_4/magicians.py
+++ chapter_4/magicians.py
@@ -1,3 +1,4 @@
 magicians = ['derrick', 'abby', 'jess']
 for magician in magicians:
-	print(magician)
+	print(f"{magician.title()}, that was a great trick!")
+	print(f"I can't wait to see your next trick, {magician.title()}.\n")
